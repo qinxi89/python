0a30dd1cc12d4dfe4651d4997de7e1ab52df2968
qinxi89 <35256082+qinxi89@users.noreply.github.com>
2019-12-14 21:38:52 +0800

    Update 元组.py

--- py_basics_01/元组.py
+++ py_basics_01/元组.py
@@ -9,16 +9,19 @@
 # @Site    : www.iqinxi.com
 # @File    : test1.py
 
+#定义一个元组，括号不是必须的，逗号才是元组的关键。
 tuple = (1,2,3,4,5,8,10)
+tuple1 = 9,10,20,64
 print(tuple)       #输出内容：(1, 2, 3, 4, 5, 8, 10)
+print(tuple1)      #输出内容：(9, 10, 20, 64)
 
 #取下标元组
-tuple1 = tuple[:3]
-print(tuple1)   #输出内容：(1, 2, 3)
+tuple2 = tuple[:3]
+print(tuple2)
 
 #元组元素不可更改
 tuple[1] = 11
-print(tuple1)     #输出内容：TypeError: 'tuple' object does not support item assignment
+print(tuple2)     #输出内容：TypeError: 'tuple' object does not support item assignment
 
 
 
