9685c38e1b9673579724fc823e9d3e3e8ca318bf
dadong <maojd@weilaicheng.com>
2019-12-14 18:54:49 +0800

    py

---
+++ py_basics_01/basics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# @Time    : 2019/12/11 9:56 下午
+# @Author  : qinxi
+# @Site    : www.iqinxi.com
+# @File    : test.py
+
+score = int(input("请输入一个分数："))
+if 100 >=  score >= 90:
+    print('A')
+elif 90 > score >=  80:
+    print('B')
+elif 80 >  score >= 60:
+    print('C')
+elif 60 > score >= 0:
+    print('D')
+else:
+    print('输入错误！')
