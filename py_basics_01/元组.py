709c6bedb68231ed6f260d260c0cdcf7c76032cc
qinxi89 <35256082+qinxi89@users.noreply.github.com>
2019-12-14 21:34:48 +0800

    Update 元组.py

--- py_basics_01/元组.py
+++ py_basics_01/元组.py
@@ -9,7 +9,16 @@
 # @Site    : www.iqinxi.com
 # @File    : test1.py
 
+tuple = (1,2,3,4,5,8,10)
+print(tuple)       #输出内容：(1, 2, 3, 4, 5, 8, 10)
 
+#取下标元组
+tuple1 = tuple[:3]
+print(tuple1)   #输出内容：(1, 2, 3)
+
+#元组元素不可更改
+tuple[1] = 11
+print(tuple1)     #输出内容：TypeError: 'tuple' object does not support item assignment
 
 
 
