62e4bf6c5090ef37f73423b7873fbe74a9241e75
qinxi89 <35256082+qinxi89@users.noreply.github.com>
2019-12-15 15:26:26 +0800

    Create for循环.py

---
+++ day01/for循环.py
@@ -0,0 +1,35 @@
+
+#猜年龄游戏：实现让用户不断的猜年龄，但只给最多3次机会，再猜不对就退出程序。
+
+my_age = 98
+count = 0
+
+while count <3:
+   user_input = int(input("input you guess num:"))
+   if user_input == my_age:
+      print("congratuation,the num you guess is correctly!")
+   elif user_input > my_age:
+      print("the num you guess is bigger!")
+   else:
+       print("the num you guess is smaller!")
+   count +=1
+else:
+    print("you are so garbage! the three times is wasted")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
