1ffa83daec8adf76ab4ee8e72ba49bf0801f7b77
qinxi89 <35256082+qinxi89@users.noreply.github.com>
2020-01-05 20:43:05 +0800

    Update 文件操作.py

--- day02/文件操作.py
+++ day02/文件操作.py
@@ -107,6 +107,28 @@ print("文件名：", fo.name)     #output:文件名： foo.txt
 fo.close()
 print("文件是否关闭：", fo.closed)        #output: True
 
+######################################### 文件定位 ###########################################
+tell()方法告诉你文件内的当前位置, 换句话说，下一次的读写会发生在文件开头这么多字节之后。
+
+seek（offset [,from]）方法改变当前文件的位置。Offset变量表示要移动的字节数。From变量指定开始移动字节的参考位置。
+如果from被设为0，这意味着将文件的开头作为移动字节的参考位置。如果设为1，则使用当前的位置作为参考位置。如果它被设为2，那么该文件的末尾将作为参考位置。
+
+
+例子：
+#read()方法
+fo = open("foo.txt", "r+")
+str = fo.read()
+print("读取的字符是：",str)    #output: www.baidu.com,very good site!
+
+#查找当前位置
+position = fo.tell()
+print("文件当前位置：",position)  #output:31
+
+#把指针重新定位到文件开头
+position = fo.seek(0,0)
+str = fo.read(10)
+print("重新读取字符串：",str)   #output:  www.baidu.
+fo.close()
 
 
 
