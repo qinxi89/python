74e63ab10ea5bc305418348d9eba93c136290d91
qinxi89 <35256082+qinxi89@users.noreply.github.com>
2020-01-19 16:52:29 +0800

    Update range循环.py

--- day01/range循环.py
+++ day01/range循环.py
@@ -1,3 +1,27 @@
+python range() 函数可创建一个整数列表，一般用在 for 循环中。
+
+函数语法
+range(start, stop[, step])
+
+参数说明：
+start: 计数从 start 开始。默认是从 0 开始。例如range（5）等价于range（0， 5）;
+stop: 计数到 stop 结束，但不包括 stop。例如：range（0， 5） 是[0, 1, 2, 3, 4]没有5
+step：步长，默认为1。例如：range（0， 5） 等价于 range(0, 5, 1)
+
+
+#示例：
+#输出数字0到9
+for r in range(10):
+    print(r)         #output: 0 1 2 3 4 5 6 7 8 9
+
+# #输入出3到9的数字
+for r1 in range(3,10):
+ print(r1)             #output:3 4 5 6 7 8 9
+
+#输出0到9，步长为2
+for r3 in range(0,10,2):
+    print(r3)           #output: 0 2 4 6 8
+
 
 
 #最简单的range循环
